9babeb36155706e086e27a31ea3a7f8a39bc73e1
h-hh19 <53941170+h-hh19@users.noreply.github.com>
2022-12-07 16:51:13 +0900

    member service test 11:30

--- src/main/java/com/daonplace/springstudy3/repository/MemberRepository.java
+++ src/main/java/com/daonplace/springstudy3/repository/MemberRepository.java
@@ -9,4 +9,6 @@ public interface MemberRepository {
     Optional<Member> findById(Long id);
     Optional<Member> findById(String name);
     List<Member> findAll();
+
+    Optional<Member> findByName(String name);
 }
